97b6a517a0483c0ddb017f92ae48621dc0616254
Jenny Lin <misslinyang91@gmail.com>
2021-08-23 11:41:09 +1000

    implemented the lockboard functionality

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const cards = document.querySelectorAll(".memory-card");
 
 let cardsIsFlipped = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+  if (lockBoard) return;
+
+  //this.classList.toggle("flip");
   this.classList.toggle("flip");
 
   if (!cardsIsFlipped) {
@@ -40,9 +44,11 @@ function disableCards() {
 }
 
 function unflipCards() {
+  lockBoard = true;
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
+    lockBoard = false;
   }, 1500);
 }
 
